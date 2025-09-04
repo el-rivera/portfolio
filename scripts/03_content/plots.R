dd544a672b92263e2f4c4197cac8350e3a377ade
el-rivera <elise.s.rivera@gmail.com>
2025-09-04 12:36:25 -0400

    added code to create a ggplot of fish total length vs depth to plots.R script

--- scripts/03_content/plots.R
+++ scripts/03_content/plots.R
@@ -1,2 +1,16 @@
-EVR628tools::create_dirs()
+# EVR628tools::create_dirs()
+library(EVR628tools)
 library(tidyverse)
+
+data("data_lionfish")
+
+plot1 = ggplot(data = data_lionfish,
+       mapping = aes(x = total_length_mm, y = depth_m)) +
+  geom_point() +
+  geom_smooth(method='lm', colour="seashell4") +
+  labs(title = "Depth Length Relationship") +
+  xlab("Total Length (mm)") +
+  ylab("Depth (m)") +
+  theme_minimal()
+
+ggsave(plot1, path = "results/img/depth_vs_length.pdf")
